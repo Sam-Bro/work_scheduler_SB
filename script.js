223d63221b8c0585f11525724008dd7a319bf61b
OrbmasWin <sambrostudent@gmail.com>
2020-03-12 18:26:55 +0800

    save text column to local and retrieve

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 var printToday = document.getElementById("todays-date");
+const saveButton = document.getElementById("btn-save");
 
+var nineStored = localStorage.getItem("input9am");
+document.getElementById("text-9am").value = nineStored
+//get todays date with moment.js
 var todaysDate = moment().format('MMMM Do YYYY');
 console.log(todaysDate);
 
+// print todays date to header 
 printToday.textContent = todaysDate;
+
+//save button function 
+$(saveButton).on("click", function() {
+   var x = document.getElementById("text-9am").value
+   localStorage.setItem("input9am", x);
+})
